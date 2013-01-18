9a27cfb258261e7474615733ddf37e8b63e9ff44
Nathaniel Blackburn <nathaniel.blackburn@outlook.com>
2013-01-18 17:54:39 +0000

    Initial release

---
+++ class.minecraft.php
@@ -0,0 +1,123 @@
+<?php
+
+	/*
+	 * @product: Minecraft Class
+	 * @description: Intergrate Minecraft within your own projects.
+	 * @author: Nathaniel Blackburn
+	 * @version: 2.0
+	 * @license: http://creativecommons.org/licenses/by/3.0/legalcode
+	 * @support: support@nblackburn.co.uk
+	 * @website: http://www.nblackburn.co.uk
+	*/
+
+class Minecraft
+{
+
+	public $account;
+
+	private function request($website, array $parameters)
+	{
+		$request = curl_init();
+		curl_setopt($request, CURLOPT_HEADER, 0);
+		curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+		if ($parameters != null) {
+			curl_setopt($request, CURLOPT_URL, $website.'?'.http_build_query($parameters, null, '&'));
+		} else {
+			curl_setopt($request, CURLOPT_URL, $website);
+		}
+		$response = curl_exec($request);
+        $details = curl_getinfo($request);
+        curl_close($request);
+        return ($details['http_code'] == 200) ? $response : null;
+	}
+
+	public function signin($username, $password, $version = 12)
+	{
+		$parameters = array('user' => $username, 'password' => $password, 'version' => $version);
+		$request = $this->request('https://login.minecraft.net/', $parameters);
+		$response = explode(':', $request);
+		if (count($response) >= 0) {
+			$this->account = array(
+				'current_version' => $response[0],
+				'correct_username' => $response[2],
+				'session_token' => $response[3],
+				'premium_account' => $this->isPremium($username),
+				'player_skin' => $this->getSkin($username),
+				'request_timestamp' => date("dmYhms", mktime(date('h'), date('m'), date('s'), date('m'), date('d'), date('y')))
+			);
+			return true;
+		}
+		return false;
+	}
+
+	public function isPremium($username)
+	{
+		$parameters = array('user' => $username);
+		return $this->request('https://www.minecraft.net/haspaid.jsp', $parameters);
+	}
+
+	public function getSkin($username)
+	{
+		if ($this->isPremium($username)) {
+			$headers = get_headers('http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
+			if ($headers[7] == 'Content-Type: image/png' || $headers[7] == 'Content-Type: application/octet-stream') {
+				return 'https://s3.amazonaws.com/MinecraftSkins/'.$username.'.png';
+			} else {
+				return 'https://s3.amazonaws.com/MinecraftSkins/char.png';
+			}
+		}
+		return false;
+	}
+
+	public function keepAlive($username, $session)
+	{
+		$parameters = array('name' => $username, 'session' => $session);
+		$this->request('https://login.minecraft.net/session', $parameters);
+		return null;
+	}
+
+	public function joinServer($username, $session, $server)
+	{
+		$parameters = array('user' => $username, 'sessionId' => $session, 'serverId' => $server);
+		$request = $this->request('http://session.minecraft.net/game/joinserver.jsp', $parameters);
+		return ($request != 'Bad Login') ? true : false;
+	}
+
+	public function checkServer($username, $server)
+	{
+		$parameters = array('user' => $username, 'serverId' => $server);
+		$request = $this->request('http://session.minecraft.net/game/checkserver.jsp', $parameters);
+		return ($request == 'YES') ? true : false;
+	}
+
+	public function renderSkin($username, $render_type = 'body', $size = 100)
+	{
+		if (in_array($render_type, array('head', 'body'))) {
+			if ($render_type == 'head') {
+				header('Content-Type: image/png');
+				$canvas = imagecreatetruecolor($size, $size);
+				$image = imagecreatefrompng($this->getSkin($username));
+				imagecopyresampled($canvas, $image, 0, 0, 8, 8, $size, $size, 8, 8);
+				return imagepng($canvas);
+			} else if($render_type == 'body') {
+				header('Content-Type: image/png');
+				$scale = $size / 16;
+				$canvas = imagecreatetruecolor(16*$scale, 32*$scale);
+				$image = imagecreatefrompng($this->getSkin($username));
+				imagealphablending($canvas, false);
+				imagesavealpha($canvas,true);
+				$transparent = imagecolorallocatealpha($canvas, 255, 255, 255, 127);
+				imagefilledrectangle($canvas, 0, 0, 16*$scale, 32*$scale, $transparent);
+				imagecopyresized  ($canvas, $image, 4*$scale,  0*$scale,  8,   8,   8*$scale,  8*$scale,  8,  8);
+				imagecopyresized  ($canvas, $image, 4*$scale,  8*$scale,  20,  20,  8*$scale,  12*$scale, 8,  12);
+				imagecopyresized  ($canvas, $image, 0*$scale,  8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12);
+				imagecopyresampled($canvas, $image, 12*$scale, 8*$scale,  47,  20,  4*$scale,  12*$scale, -4,  12);
+				imagecopyresized  ($canvas, $image, 4*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12);
+				imagecopyresampled($canvas, $image, 8*$scale,  20*$scale, 7,   20,  4*$scale,  12*$scale, -4,  12);
+				return imagepng($canvas);
+			}
+		}
+		return false;
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,106 @@
+# Minecraft Class
+
+This class was developed to provide a set of functions to integrate Minecraft within your projects. Under no circumstances
+are you permitted to use this for malicious purposes.
+
+#### Example Usage
+
+```php
+<?php
+    include('class.minecraft.php');
+	if ($minecraft->signin('username', 'password') == true) {
+		foreach($minecraft->account as $field => $value) {
+			echo($field.'->'.$value.'<br>');
+		}
+	} else {
+		echo('Error!');
+	}
+?>
+```
+
+---
+
+#### Login
+
+```php
+$minecraft->login('username', 'password', 'version');
+```
+
+This function is used to login to your Minecraft account, taking 3 parameters: username, password and the version of the minecraft
+launcher which currently defaults to **12**. This returns **true** if the user was successfully authenticated and **false**
+otherwise. An array is also set upon successful login which contains the following...
+
+* **current_version**: The current version of the games resources.
+* **correct_username**: A correctly formatted username.
+* **session_token**: The current session used to login to the Minecraft client.
+* **premium_account**: If the account specified is of premium status.
+* **player_skin**: The url to the users custom skin, if applicable.
+* **request_timestamp**: the timestamp for the request which is formatted as **DDMMYYYYHHMMSS** in local server time.
+
+```php
+$minecraft->account();
+```
+
+This array can be called using the function above once the user has logged in to their account otherwise **null** will be returned
+when calling this function.
+
+---
+
+#### Is Premium
+
+```php
+$minecraft->is_premium('username');
+```
+
+This function determines if the user specified has a premium account or not. It takes a single parameter of **username** and
+returns **true** if a premium account was detected and **false** otherwise.
+
+---
+
+#### Get Skin
+
+```php
+$minecraft->get_skin('username');
+```
+
+This function firstly checks the user specified has a premium account, then returns a url to the skin file for that user if
+a custom skin was found. It takes a single parameter which is the **username** of the user you which to get the skin file of.
+
+---
+
+#### Keep Alive
+
+```php
+$minecraft->keep_alive('username', 'session');
+```
+
+This function is used to keep the users current session alive, this command needs sending to the Minecraft servers every 600
+ticks (60 seconds) otherwise the user is signed out. It takes 2 parameters, the **username** and **session** returned from signing
+in to your account, this can be obtained with the following code...
+
+---
+
+#### Account
+
+```php
+$minecraft->account['session_token'];
+```
+
+The function returns **null** as the Minecraft server doesnt appear to throw back any exceptions or acknowledgement of the request.
+
+---
+
+#### Render Skin
+
+```php
+$minecraft->render_skin($username, $render_type, $size);
+```
+
+This function renders the specified player's skin. It takes 3 parameters, the first being the **username** of the player whos skin you wish to render,
+the **render_type** which can either be **head** OR **body** and the **size** you would like the rendered image to be. Please note when rendering
+the full body, the image width is half the size of the image height. You can include this function directly inside a **img** tag by using the following
+example...
+
+```html
+<img src="<?php echo($minecraft->render_skin('nblackburn', 'head', 100)) ?>" width="100" height="100">
+```
